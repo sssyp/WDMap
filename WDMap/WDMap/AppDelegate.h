3c40804706b11223ec2e8d0d625cee4abccfdf05
wbb <1377585755@qq.com>
2021-01-29 18:47:17 +0800

    项目初始化

---
+++ WDMap/WDMap/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  WDMap
+//
+//  Created by wbb on 2021/1/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ WDMap/WDMap/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  WDMap
+//
+//  Created by wbb on 2021/1/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
